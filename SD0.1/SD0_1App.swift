c0ebd448eaeaee9eed157e5e94aabe774c1e6463
varhuman <770420614@qq.com>
2023-03-15 19:50:14 +0800

    Initial Commit

---
+++ SD0.1/SD0_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SD0_1App.swift
+//  SD0.1
+//
+//  Created by CreepOnSky on 2023/3/15.
+//
+
+import SwiftUI
+
+@main
+struct SD0_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
